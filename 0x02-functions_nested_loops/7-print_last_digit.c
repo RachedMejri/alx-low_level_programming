b5cc7c905b541e4d8717ba47563238d184d0015d
root <root@DELL-PC>
2023-07-18 23:21:58 +0100

    lol

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Prints the alphabet in lowercase 10 times
+ *
+ * @n: al carcter
+ *
+ * Return: On success, returns 1.
+ *         On 0, on error
+ */
+
+int print_last_digit(int n)
+{
+
+	return (n % 10);
+
+}
