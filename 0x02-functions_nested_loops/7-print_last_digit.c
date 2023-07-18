8a06d0429f01e54a7619bf74cffd918152e575fb
root <root@DELL-PC>
2023-07-18 23:46:13 +0100

    lol

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,18 +4,23 @@
  * print_last_digit - Prints and returns the last digit of a number
  * @n: The number
  *
- * Return: The value of the last digit
+ * Return: The last digit of the number
  */
 int print_last_digit(int n)
 {
 	int last_digit;
 
+
 	if (n < 0)
 		n = -n;
 
+
 	last_digit = n % 10;
 
+
 	_putchar('0' + last_digit);
-	
+
+
 	return (last_digit);
 }
+
