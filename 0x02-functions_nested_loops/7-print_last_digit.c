1c3b63a7a625c3b18123aac1d2dd493fd5c49cf9
root <root@DELL-PC>
2023-07-18 23:52:42 +0100

    lol

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,17 +10,12 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-
-	if (n < 0)
-		n = -n;
-
-
-	last_digit = n % 10;
-
+	if (n >= 0)
+		last_digit = n % 10;
+	else
+		last_digit = -n % 10;
 
 	_putchar('0' + last_digit);
 
-
 	return (last_digit);
 }
-
