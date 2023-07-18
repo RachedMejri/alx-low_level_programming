c102dce3c06686c30d1aa2e0d5033e2299dbb912
root <root@DELL-PC>
2023-07-18 22:41:29 +0100

    lol

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - Prints the alphabet in lowercase 10 times
+ */
+
+int _islower(int c)
+{
+
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
