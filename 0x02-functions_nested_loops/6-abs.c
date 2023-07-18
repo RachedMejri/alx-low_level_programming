cf5650ede66b709e38d285bddfb2e7f0a558b748
root <root@DELL-PC>
2023-07-18 23:18:54 +0100

    lol

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Prints the alphabet in lowercase 10 times
+ *
+ * @n: al carcter
+ *
+ * Return: On success, returns 1.
+ *         On 0, on error
+ */
+
+int _abs(int n)
+{
+
+	return (abs(n));
+
+}
