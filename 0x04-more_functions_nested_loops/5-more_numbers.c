7545c02cae0d67cc4a98b623217536ce6f15b28b
root <root@DELL-PC>
2023-07-20 19:47:03 +0100

    lol

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints from 0 to 9 except 2 4
+ *
+ *
+ * Return: the multiplication of both a and b
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (i = 0 ; i <= 14 ; i++)
+			_putchar(i + 48);
+
+		_putchar('\n');
+	}
+
+
+
+}
