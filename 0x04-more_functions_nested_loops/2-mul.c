4ed8de97bcb0e90aabe4203d6da2126f8f48673a
root <root@DELL-PC>
2023-07-20 19:12:22 +0100

    lol

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multplie 2 numbers
+ *
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the multiplication of both a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
