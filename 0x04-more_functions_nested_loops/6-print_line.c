0ad6d91aca33df08cbaba95028c611b6c0df89c2
root <root@DELL-PC>
2023-07-20 21:15:13 +0100

    off

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,25 @@
 #include "main.h"
+
 /**
  * print_line - line printing
  *
- *@n: number
+ * @n: number
  *
- * print -
+ * print a line of underscores n times
  */
-
 void print_line(int n)
 {
-
 	int i = 0;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-
-		while (i <= n)
+		while (i < n)
 		{
-			_putchar(95);
+			_putchar('_');
 			i++;
 		}
 		_putchar('\n');
