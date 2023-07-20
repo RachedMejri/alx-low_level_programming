c0bced78f38b7dbb12cbf7f661e5e1c6049d8271
root <root@DELL-PC>
2023-07-20 21:05:27 +0100

    check system sucks

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,19 @@
 
 void print_line(int n)
 {
-	int i;
 
-	if (n > 0)
+	int i = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		for (i = 0 ; i <= n ; i++)
-	{
-			_putchar('_');
-	}
 
+		while (i <= n)
+		{
+			_putchar(95);
+			i++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-
 }
