59e9db48da60965c44fa74f45d6c5c90c18bb64e
root <root@DELL-PC>
2023-07-20 20:10:05 +0100

    lol

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,21 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int first_num;
 
 	for (i = 0 ; i <= 10 ; i++)
 	{
 		for (j = 0 ; j <= 14 ; j++)
-			_putchar(j + 48);
+		{
+			first_num = j;
+
+			if (j > 9)
+			{
+				_putchar(49);
+				first_num= first_num - 10;
+			}
+			_putchar(first_num + 48);
+		}
 
 		_putchar('\n');
 	}
