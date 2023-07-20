ca2256f96dca44f7069c438acdc367f90a4f34b1
root <root@DELL-PC>
2023-07-20 20:48:14 +0100

    lol

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,33 +1,21 @@
 #include "main.h"
-/**
- * more_numbers - print alot of numbers
- *
- *
- * Return: from 0 to 14 10 times
- */
 
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ */
 void more_numbers(void)
 {
+	int x, y;
 
-	int x;
-	int y;
-	int a;
-
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y <= 14 ; y++)
+		for (y = 0; y <= 14; y++)
 		{
-
-			a = y;
 			if (y > 9)
-			{
-				_putchar(1 + 48);
-				a = y % 10;
-			}
-			_putchar(a + 48);
+				_putchar('0' + (y / 10));
+			_putchar('0' + (y % 10));
 		}
 		_putchar('\n');
-
 	}
-
 }
+
