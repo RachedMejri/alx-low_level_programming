b97a26689b6980890eed78098221e1010d31e66e
root <root@DELL-PC>
2023-07-20 21:09:21 +0100

    check system sucks

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,27 +1,21 @@
 #include "main.h"
-/**
- * print_line - line printing
- *
- *@n: number
- *
- * print -
- */
 
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character '_' should be printed
+ *
+ * If n is 0 or less, the function only prints '\n'.
+ */
 void print_line(int n)
 {
-
-	int i = 0;
-
 	if (n <= 0)
-		_putchar('\n');
-	else
 	{
-
-		while (i <= n)
-		{
-			_putchar("_");
-			i++;
-		}
 		_putchar('\n');
+		return;
 	}
+
+	for (int i = 0; i < n; i++)
+		_putchar('_');
+
+	_putchar('\n');
 }
