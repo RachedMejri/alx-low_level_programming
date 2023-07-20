13ee359eb24cf143217bd9fc81673e2fdae38302
root <root@DELL-PC>
2023-07-20 21:11:57 +0100

    :(

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,27 @@
 #include "main.h"
-
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character '_' should be printed
+ * print_line - line printing
  *
- * If n is 0 or less, the function only prints '\n'.
+ *@n: number
+ *
+ * print -
  */
+
 void print_line(int n)
 {
+
+	int i = 0;
+
 	if (n <= 0)
-	{
 		_putchar('\n');
-		return;
+	else
+	{
+
+		while (i <= n)
+		{
+			_putchar(95);
+			i++;
+		}
+		_putchar('\n');
 	}
-
-	for (int i = 0; i < n; i++)
-		_putchar('_');
-
-	_putchar('\n');
 }
