831ce8c4f3fd26309c75c9eece5de367699b8564
root <root@DELL-PC>
2023-07-20 19:22:12 +0100

    lol

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints from 0 to 9
+ *
+ *
+ * Return: the multiplication of both a and b
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		_putchar(i);
+}
