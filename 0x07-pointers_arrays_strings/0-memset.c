dde1539180501b748b458d88eb13de31bd031c5c
root <root@DELL-PC>
2023-07-31 12:02:30 +0100

    lol

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - 3abi al plys pil b
+ * @s: Pointer to the array of integers.
+ * @b: Number of elements in the array.
+ * @n: 9dh min w7da
+ * Return: Always 0.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	memset(s, b, n);
+
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
+
+/*
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+void reverse_array(int *a, int n);
+
+*/
