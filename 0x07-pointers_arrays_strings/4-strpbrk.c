cb46d1d102cce8c9f582cf949ef98548a8ac57cf
root <root@DELL-PC>
2023-07-31 14:08:41 +0100

    lol

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - nfs al 7aja
+ * @s: al string
+ * @accept: nfs 7aja mara o5ra
+ *
+ * Return: copy coller li defecntion mt3 function
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+
+	return (NULL);
+}
