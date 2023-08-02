07dc7c8c36b5264a4507816dc87705b6d72511e8
root <root@DELL-PC>
2023-08-02 19:23:57 +0100

    lol

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *_sqrt_recursion - calculet the naturual square root of a number
+ * @n: al nmbre
+ *
+ * Return: return square root of a number if the square root is a nat number
+ *                 else -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
+		return (OldWayCal(n, 2));
+}
+
+/**
+ *OldWayCal - calcule the square root the old way
+ * @nom: al nmbre
+ * @fac: al fac
+ * Return: the square root
+ */
+
+int OldWayCal(int nom, int fac)
+{
+	if (nom == fac)
+		return (-1);
+	else if (fac * fac == nom)
+		return (fac);
+	else
+		return (OldWayCal(nom, fac + 1));
+}
