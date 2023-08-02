e342ad9de950ac65f73703775e6940702ee78ea1
root <root@DELL-PC>
2023-08-02 20:22:44 +0100

    lol

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * is_prime_number - check if the number is prime number
+ * @n: al nmbre
+ *
+ * Return: return 1 if the number is prime, else 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (OldWayCal(n, 2));
+}
+
+/**
+ * OldWayCal - check if the number is prime number
+ * @nom: al nmbre
+ * @fac: al fac
+ *
+ * Return: return 1 if the number is prime, else 0
+ */
+int OldWayCal(int nom, int fac)
+{
+	if (nom == fac)
+		return (1);
+
+	if (nom % fac == 0)
+		return (0);
+
+	return (OldWayCal(nom, fac + 1));
+}
