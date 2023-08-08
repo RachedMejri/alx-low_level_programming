80f80728a9bb76504e6c9ad682edac83baa69c60
root <root@DELL-PC>
2023-08-08 20:10:56 +0100

    lol

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,11 +13,14 @@ void free_grid(int **grid, int height)
 	if (grid == NULL || height <= 0)
 		return;
 
-	int i;
-	for (i = 0; i < height; i++)
+	for (int i = 0; i < height; i++)
 	{
-		free(grid[i]);
+		if (grid[i] != NULL)
+		{
+			free(grid[i]);
+			grid[i] = NULL;
+		}
 	}
-	
+
 	free(grid);
 }
