80433d715a552dffed331b7ba871009d177235e1
root <root@DELL-PC>
2023-08-08 19:47:46 +0100

    lol

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creating 2d aray
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Pointer to the newly created 2D array,
+ * or NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	int **grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (int i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (int j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+
+		for (int j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
