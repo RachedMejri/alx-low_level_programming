20f77b4d1e7bd5a36ce4ff8cc35e0023f19eeba4
root <root@DELL-PC>
2023-08-10 15:28:02 +0100

    lol

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,21 +2,18 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - Write a function that allocates memory using malloc.
- * @b: size
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to be allocated
  *
- * Return: the location,
- * or exit 98
+ * Return: pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *ptr;
 
-	p = malloc(b);
+	ptr = malloc(b);
 
-	if (p == NULL)
+	if (ptr == NULL)
 		exit(98);
-
 	return (ptr);
 }
