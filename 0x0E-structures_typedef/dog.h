41ffd324ffac1909df96059abe9fb9f7f6e34940
root <root@DELL-PC>
2023-08-14 22:11:18 +0100

    lol

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a new type that defines a dog
+ * @name: ism
+ * @age: 3mor
+ * @owner: who own a god
+ *
+ * Description: lil klab lili jaya fil most9bl
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-#endif
+#endif /* DOG_H */
+
