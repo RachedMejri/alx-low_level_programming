b451754d0d159ed6f5c69fb27dbc4cb3ae0bd097
root <root@DELL-PC>
2023-08-14 23:04:21 +0100

    lol

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,9 @@ struct dog
 	char *owner;
 };
 
+/* define a new type dog_t as a new name for the type struct dog */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
