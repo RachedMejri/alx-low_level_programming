3c4b5da8e9bb39ec1c74354182f6498273faefac
root <RachedMejri/alx-low_level_programming.git>
2023-08-16 19:19:23 +0100

    lol

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: array to search in
+ * @size: size of the array
+ * @cmp: comparison function to use
+ *
+ * Return: index of the first element for which cmp does not return 0,
+ * or -1 if no element is found or size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+
+	return (-1);
+}
