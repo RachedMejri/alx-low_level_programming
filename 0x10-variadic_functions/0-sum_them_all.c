fd36b780ad66cee2064a94bb05808626a6f943d2
MejriRached <majri.rached@gmail.com>
2023-08-17 15:39:03 +0100

    i cant use unsinged int i in rule 89

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,10 +11,10 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int i;
+	int sum = 0;
 
 	va_start(args, n);
-
-	int sum = 0;
 
 	if (n == 0)
 	{
@@ -22,7 +22,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	}
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		int x = va_arg(args, int);
 
