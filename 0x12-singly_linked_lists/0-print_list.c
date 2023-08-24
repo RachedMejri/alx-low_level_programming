533bc66ecb29f8c8995a19dcd9ca9eb11f06bddc
RachedMejri <majri.rached@gmail.com>
2023-08-24 17:39:35 +0100

    count was not intialized

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_list - print all and calculet the size of linked
+ * print_list -  a function that prints all the elements of a list_t list.
  *
  *@h: the head of the linked arrya
  *
- * Return: size of the linked list
+ * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	int count;
+	int count = 0;
 
 	if (h == NULL)
 		return (0);
