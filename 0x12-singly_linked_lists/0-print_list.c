6a2184347ebe009ffd97aeb6f87a3f1d99c1e6e7
RachedMejri <majri.rached@gmail.com>
2023-08-24 17:49:31 +0100

    changed the structure

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,21 +9,23 @@
  */
 size_t print_list(const list_t *h)
 {
-	int count = 1;
+	int count;
 
 	if (h == NULL)
 		return (0);
 
-	while (h->next != NULL)
+	for (count = 0; h->next != NULL; count++)
 	{
-		count++;
 		if (h->str == NULL)
 			printf("[%u] %s\n", h->len, "(nil)");
-
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
+		}
+
 		h = h->next;
 	}
+
 	printf("[%u] %s\n", h->len, "(nil)");
 	return (count);
 }
