bbfc1751ff8d4cfd343ab802d6110e5bc55e9b4a
RachedMejri <majri.rached@gmail.com>
2023-08-24 18:22:45 +0100

    first try now

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,19 @@
 #include "lists.h"
+#include <stdio.h>
 /**
- * list_len - function that returns the number of elements in a linked list_t list.
+ * list_len - function that returns the number of elements in a linked list_t l
  * @h: pointer to the head of the list
  *
  * Return: size of the linklist
  */
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        h = h->next;
-        count++;
-    }
-    return (count);
+	if (h == NULL)
+		return (0);
+
+	for (count = 0; h->next != NULL; count++)
+		h = h->next;
+	return (count);
 }
