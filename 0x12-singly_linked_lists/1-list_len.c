b84ea1912a7cd69c5ca22cd07d0f85faee318f04
RachedMejri <majri.rached@gmail.com>
2023-08-24 18:25:14 +0100

    second otiration

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,10 +10,10 @@ size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-
-	for (count = 0; h->next != NULL; count++)
+	while (h != NULL)
+	{
+		count++;
 		h = h->next;
+	}
 	return (count);
 }
