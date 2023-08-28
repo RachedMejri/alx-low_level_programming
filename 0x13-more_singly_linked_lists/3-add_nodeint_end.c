e4b80c91f607b79ec9cb4db3e9925f9e33e42bda
RachedMejri <majri.rached@gmail.com>
2023-08-28 19:03:23 +0100

    3 task after checking betty again

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adding a new node to link at the endthistime
+ * @head: haeder b pointer pointer
+ * @n: value de node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newnode = malloc(sizeof(listint_t));
+	listint_t *temp;
+
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = NULL;
+	if (*head == NULL)
+		*head = newnode;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = newnode;
+	}
+	return (newnode);
+}
