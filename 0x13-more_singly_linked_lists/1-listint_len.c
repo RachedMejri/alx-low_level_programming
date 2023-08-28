e81a8facb7cc0fea3809a0e89787ff3b5efe0e7d
RachedMejri <majri.rached@gmail.com>
2023-08-28 18:13:17 +0100

    simmilar one  to the  one did 2 days ago but enhan

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - nombre
+ * @h: pointer lil lista
+ *
+ * Return: the number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+    size_t count = 0;
+
+    while (h)
+    {
+        count++;
+        h = h->next;
+    }
+    return (count);
+}
