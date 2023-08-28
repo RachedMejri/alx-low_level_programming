46da42d66c9450175f24f3ff5a70e4d57d859ae7
RachedMejri <majri.rached@gmail.com>
2023-08-28 19:08:08 +0100

    lol did it from the start while i forgot .h again

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,29 +1,30 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adding a new node to link at the endthistime
- * @head: haeder b pointer pointer
- * @n: value de node
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: double pointer to the head of the list
+ * @n: value of the new node
  *
  * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newnode = malloc(sizeof(listint_t));
-	listint_t *temp;
+	listint_t *temporor;
 
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
+
 	if (*head == NULL)
 		*head = newnode;
 	else
 	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = newnode;
+		temporor = *head;
+		while (temporor->next != NULL)
+			temporor = temporor->next;
+		temporor->next = newnode;
 	}
 	return (newnode);
 }
