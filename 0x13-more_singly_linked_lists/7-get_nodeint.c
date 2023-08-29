2c91e225737ece1e8eda958e9b9565d294061df2
RachedMejri <majri.rached@gmail.com>
2023-08-29 17:15:44 +0100

    i cant find my cat i shouldve feed him im so sad

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - return the data in index place
+ * @head: head
+ * @index: index lil plsa
+ *
+ * Return: null or place data
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int i;
+
+	for (i = 0; i < index && head != NULL; i++)
+		head = head->next;
+	return (head);
+}
