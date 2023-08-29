6cd79f371bd6e49190c44ab0c842296c94c9209e
RachedMejri <majri.rached@gmail.com>
2023-08-29 16:33:00 +0100

    checking why 3 not working

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,22 +9,21 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newnode = malloc(sizeof(listint_t));
-	listint_t *temporor;
+	listint_t *newnode, *tomp;
 
+	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
-
 	if (*head == NULL)
 		*head = newnode;
 	else
 	{
-		temporor = *head;
-		while (temporor->next != NULL)
-			temporor = temporor->next;
-		temporor->next = newnode;
+		tomp = *head;
+		while (tomp->next != NULL)
+			tomp = tomp->next;
+		tomp->next = newnode;
 	}
 	return (newnode);
 }
