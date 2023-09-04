fb4929b4918fa1120e372e1c7d06475f5b104e85
RachedMejri <majri.rached@gmail.com>
2023-09-04 18:54:45 +0100

    same code just asked the aii to fix some problems

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,13 +2,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 #define BUFFER_SIZE 1024
 #define USAGE "Usage: cp file_from file_to\n"
 #define READ_ERROR "Error: Can't read from file %s\n"
 #define WRITE_ERROR "Error: Can't write to %s\n"
 #define CLOSE_ERROR "Error: Can't close fd %d\n"
 
-void exiterror(char *message, int error_code, ...);
+void exit_with_error(char *message, int error_code, ...);
 
 /**
  * main - Entry point
@@ -19,27 +20,34 @@ void exiterror(char *message, int error_code, ...);
  */
 int main(int argc, char *argv[])
 {
-	int ffrom, tto;
-	ssize_t redern;
-	char temp[BUFFER_SIZE];
+	int fd_from, fd_to;
+	ssize_t n_read;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-		exiterror(USAGE, 97);
-	ffrom = open(argv[1], O_RDONLY);
-	if (ffrom == -1)
-		exiterror(READ_ERROR, 98, argv[1]);
-	tto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (tto == -1)
-		exiterror(WRITE_ERROR, 99, argv[2]);
-	while ((redern = read(ffrom, temp, BUFFER_SIZE)) > 0)
-		if (write(tto, temp, redern) != redern)
-			exiterror(WRITE_ERROR, 99, argv[2]);
-	if (redern == -1)
-		exiterror(READ_ERROR, 98, argv[1]);
-	if (close(ffrom) == -1)
-		exiterror(CLOSE_ERROR, 100, ffrom);
-	if (close(tto) == -1)
-		exiterror(CLOSE_ERROR, 100, tto);
+		exit_with_error(USAGE, 97);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		exit_with_error(READ_ERROR, 98, argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		exit_with_error(WRITE_ERROR, 99, argv[2]);
+
+	while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+		if (write(fd_to, buffer, n_read) != n_read)
+			exit_with_error(WRITE_ERROR, 99, argv[2]);
+
+	if (n_read == -1)
+		exit_with_error(READ_ERROR, 98, argv[1]);
+
+	if (close(fd_from) == -1)
+		exit_with_error(CLOSE_ERROR, 100, fd_from);
+
+	if (close(fd_to) == -1)
+		exit_with_error(CLOSE_ERROR, 100, fd_to);
+
 	return (0);
 }
 
@@ -48,7 +56,7 @@ int main(int argc, char *argv[])
  * @message: The error message to print
  * @error_code: The error code to exit with
  */
-void exiterror(char *message, int error_code, ...)
+void exit_with_error(char *message, int error_code, ...)
 {
 	va_list args;
 
